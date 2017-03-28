6123b21760c8bb959e5081cf39f2b5dbdd1b7573
ChalkPE <chalk@chalk.pe>
2017-03-28 20:35:19 +0900

    Fixed duplicate middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
 
 import routes from './routes';
 import logger from './logger';
 
 const app = new Koa();
+const port = process.env.PORT || 8888;
 
 app
     .use(logger())
-    .use(routes())
-    .listen(8888);
+    .use(bodyParser())
+    .use(routes());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import compose from 'koa-compose';
-import bodyParser from 'koa-bodyparser';
 
 import rootRouter from './root';
 import authRouter from './auth';
@@ -11,7 +10,6 @@ const routes = [
     examRouter,
 ];
 
-export default () => {
-    let middlewares = routes.map(r => [r.routes(), r.allowedMethods()]);
-    return compose([bodyParser()].concat(...middlewares));
-}
+export default () => compose([].concat(
+    ...routes.map(r => [r.routes(), r.allowedMethods()])
+));
