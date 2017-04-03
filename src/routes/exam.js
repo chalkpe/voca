d69ec23474b39ef265b4fa725d42eb0e3579ec73
ChalkPE <chalk@chalk.pe>
2017-04-03 21:57:10 +0900

    Updated backend router: exam, learn

--- src/routes/exam.js
+++ src/routes/exam.js
@@ -1,8 +1,15 @@
 import Router from 'koa-router'
+
+import Day from '../models/Day'
+
 const router = new Router({ prefix: '/exam' })
 
-router.get('/', async (ctx, next) => {
-  ctx.body = 'World'
-})
+router.get('/:book/:day', async (ctx, next) =>
+  (ctx.body = await Day.aggregate(
+    { $match: { book: ctx.params.book, id: parseInt(ctx.params.day) } },
+    { $unwind: '$words' },
+    { $sample: { size: 10 } },
+    { $group: { _id: '$_id', problems: { $push: '$words' } } }
+  )))
 
 export default router
--- src/routes/learn.js
+++ src/routes/learn.js
@@ -1,10 +1,17 @@
 import Router from 'koa-router'
+
+import Day from '../models/Day'
 import Book from '../models/Book'
 
 const router = new Router({ prefix: '/learn' })
 
-router.get('/', async (ctx, next) => {
-  ctx.body = await Book.find()
-})
+router.get('/', async (ctx, next) =>
+  (ctx.body = await Book.find().sort('id')))
+
+router.get('/:book', async (ctx, next) =>
+  (ctx.body = await Book.findOne({ id: ctx.params.book })))
+
+router.get('/:book/:day', async (ctx, next) =>
+  (ctx.body = await Day.findOne({ book: ctx.params.book, id: ctx.params.day })))
 
 export default router
