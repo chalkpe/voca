b650ed8b753ca6c9d6e6dead9ecab43778429613
ChalkPE <chalk@chalk.pe>
2017-04-04 18:34:48 +0900

    Basic auth implemented

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose'
 
 const schema = mongoose.Schema({
-  // TODO
+  username: { type: String, unique: true },
+
+  token: String,
+  name: String,
+  email: String
 })
 
 export default mongoose.model('User', schema)
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,30 +1,33 @@
 import axios from 'axios'
 import crc32 from 'crc-32'
 import sha256 from 'sha256'
-import sprintf from 'sprintf'
 
 import Router from 'koa-router'
 import * as config from '../config'
 
+const auth = {
+  username: config.DIMIGO_API_ID,
+  password: config.DIMIGO_API_PW
+}
+
 const router = new Router({ prefix: '/auth' })
 
-router.post('/', async (ctx, next) => {
+router.post('/', async (ctx) => {
   const { username, password } = ctx.request.body
+  const uri = config.DIMIGO_API_HOST + '/v1/users/identify'
 
-  const c = sprintf('%010u', crc32.str(password)) // TODO: PHP crc32() Compatibility
-  const hash = '@' + sha256(password + c)
-
-  const url = config.DIMIGO_API_HOST + '/v1/users/identify'
-  console.log(url, c, hash)
+  const crc = '0000000000' + (crc32.str(password) >>> 0)
+  const hash = '@' + sha256(password + crc.substr(-10, 10))
 
   try {
-    const { data } = await axios.get(url, {
-      params: { username, password },
-      auth: { username: config.DIMIGO_API_ID, password: config.DIMIGO_API_PW }
+    const { data } = await axios.get(uri, {
+      auth, params: { username, password: hash }
     })
+
     ctx.body = data
-  } catch (e) {
-    ctx.body = e.response.data
+    // TODO: session data (JWT)
+  } catch ({ response: { data } }) {
+    ctx.throw(data.status || 401, data)
   }
 })
 
